1be07986bdccc1b22d2fea612543d98341ffffe7
lamartinecabral <ingloryon@gmail.com>
2023-06-18 13:57:47 +0000

    build

--- iuai.d.ts
+++ iuai.d.ts
@@ -1,24 +1,21 @@
-declare type Tags = keyof HTMLElementTagNameMap;
-declare type Elem<T extends Tags> = HTMLElementTagNameMap[T];
-declare type EvTypes = keyof HTMLElementEventMap;
-declare type Ev<T extends EvTypes> = HTMLElementEventMap[T];
+type Tags = keyof HTMLElementTagNameMap;
+type Elem<T extends Tags> = HTMLElementTagNameMap[T];
+type Attr<T extends Tags> = Partial<Omit<Elem<T>, "style"> & {
+    style: Partial<CSSStyleDeclaration>;
+}>;
 declare function elem<T extends Tags>(tag: T): Elem<T>;
 declare function elem<T extends Tags>(tag: T, text: string): Elem<T>;
-declare function elem<T extends Tags>(tag: T, attributes: Partial<Elem<T>>): Elem<T>;
-declare function elem<T extends Tags, W extends HTMLElement | string>(tag: T, children: W[]): Elem<T>;
-declare function elem<T extends Tags>(tag: T, attributes: Partial<Elem<T>>, text: string): Elem<T>;
-declare function elem<T extends Tags, W extends HTMLElement | string>(tag: T, attributes: Partial<Elem<T>>, children: W[]): Elem<T>;
+declare function elem<T extends Tags>(tag: T, attributes: Attr<T>): Elem<T>;
+declare function elem<T extends Tags>(tag: T, children: Array<HTMLElement | string>): Elem<T>;
+declare function elem<T extends Tags>(tag: T, attributes: Attr<T>, text: string): Elem<T>;
+declare function elem<T extends Tags>(tag: T, attributes: Attr<T>, children: Array<HTMLElement | string>): Elem<T>;
 declare namespace elem {
     var get: typeof elemGet;
     var getChild: typeof elemGetChild;
     var getParent: typeof elemGetParent;
 }
-declare function elemGet<T extends Tags = "main">(id: string): Elem<T>;
-declare function elemGet<T extends Tags>(tag: T, id: string): Elem<T>;
-declare function elemGetChild<T extends Tags = "main">(id: string): Elem<T>;
-declare function elemGetChild<T extends Tags>(tag: T, id: string): Elem<T>;
-declare function elemGetParent<T extends Tags = "main">(id: string): Elem<T>;
-declare function elemGetParent<T extends Tags>(tag: T, id: string): Elem<T>;
+declare function elemGet<T extends Tags = "main">(id: string, tag?: T): Elem<T>;
+declare function elemGetChild<T extends Tags = "main">(id: string, tag?: T): Elem<T>;
+declare function elemGetParent<T extends Tags = "main">(id: string, tag?: T): Elem<T>;
 export { elem };
 export declare function style<T extends Partial<CSSStyleDeclaration>>(selector: string, properties: T): CSSStyleRule;
-export declare function handle<W extends HTMLElement, T extends EvTypes>(element: W, eventType: T, handler: (ev: Ev<T>) => void): () => void;
